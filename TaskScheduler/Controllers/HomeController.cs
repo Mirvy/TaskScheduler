2228fef293cc9252667fea44b832cae19ec6e602
Mike <michael.yelverton89@gmail.com>
2022-10-04 12:58:36 -0700

    Added a basic home controller to verify business layer services can be provided through DI.

---
+++ TaskScheduler/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using DutyBusinessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskScheduler.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IDutyService _dutyService;
+        private ITeamService _teamService;
+        private IEmployeeService _employeeService;
+        private IProjectService _projectService;
+
+        public HomeController(IDutyService dutyService, ITeamService teamService, IEmployeeService employeeService, IProjectService projectService)
+        {
+            _dutyService = dutyService;
+            _teamService = teamService;
+            _employeeService = employeeService;
+            _projectService = projectService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
